1e1be0b458143c3beb75e478d54f3efb054e2d90
Richard Arnold <richard@ambercouch.co.uk>
2021-06-04 07:06:09 +0100

    CDC - Update shortcode to check that $post exists

--- lib/ac-shortcode.php
+++ lib/ac-shortcode.php
@@ -48,51 +48,58 @@ function cdc_shortcode_video_block($atts) {
       if ($query->have_posts()) :
         while ($query->have_posts()) :
           $query->the_post();
-          $output .= '<div class="video-preview__video-thumb with atts">';
-          $output .= '<div class="video-thumb">';
-          $output .= '<a class="video-link " href="#modal-'.$vid_id.'">';
-          //$output .= '<a class="video-link cdc_ajax" href="#modal">';
-          if (has_post_thumbnail($post->ID)) {
-            $output .= get_the_post_thumbnail($post->ID);
-          } else {
+            if(isset($post))
+            {
+                $output .= '<div class="video-preview__video-thumb with atts ac2021">';
+                $output .= '<div class="video-thumb">';
+                $output .= '<a class="video-link " href="#modal-' . $vid_id . '">';
+                //$output .= '<a class="video-link cdc_ajax" href="#modal">';
 
-            $output .= '<svg preserveAspectRatio="none" class="icon video-link__icon--play ">';
-            $output .= '<use xlink:href = "#icon-play" />';
-            $output .= '</svg>';
-            $output .= '<small class="video-link__play-text">Play</small>';
-          }
-            $output .='</a>';
+                if (has_post_thumbnail($post->ID))
+                {
+                    $output .= get_the_post_thumbnail($post->ID);
+                } else
+                {
+
+                    $output .= '<svg preserveAspectRatio="none" class="icon video-link__icon--play ">';
+                    $output .= '<use xlink:href = "#icon-play" />';
+                    $output .= '</svg>';
+                    $output .= '<small class="video-link__play-text">Play</small>';
+                }
+
+                $output .= '</a>';
 
 
+                $output .= '</div>';
+                $output .= '<h6 class="video-preview__video-title">' . get_field('video_title', $post->ID) . '</h6>';
+                $output .= '</div>';
+                $output .= (is_user_logged_in() == false) ? '<div class="remodal" data-remodal-id="modal-' . $vid_id . '">' : '<div class="remodal" data-remodal-id="modal-' . $vid_id . '">';
+                $output .= '<div id="ajax-box"  class="iframe video ">';
+                $output .= '<h1 class="video__title">';
 
-            $output .= '</div>';
-            $output .= '<h6 class="video-preview__video-title">' . get_field('video_title', $post->ID) . '</h6>';
-            $output .= '</div>';
-            $output .= (is_user_logged_in() == false)? '<div class="remodal" data-remodal-id="modal-'.$vid_id.'">' : '<div class="remodal" data-remodal-id="modal-'.$vid_id.'">';
-            $output .= '<div id="ajax-box"  class="iframe video ">';
-            $output .= '<h1 class="video__title">';
+                $output .= '<span class="title" >' . get_field('video_title') . '</span>';
+                $output .= '</h1>';
 
-            $output .= '<span class="title" >'. get_field('video_title') .'</span>';
-            $output .= '</h1>';
+                if (get_field('swf_video', $post->ID))
+                {
+                    $output .= '';
+                    $output .= '<object type="application/x-shockwave-flash"';
+                    $output .= 'data="' . get_field('swf_video', $post->ID) . '"';
+                    $output .= 'width="' . get_field('swf_width', $post->ID) . '" height="' . get_field('swf_height', $post->ID) . '">';
+                    $output .= '<param name="movie" value="' . get_field('swf_video', $post->ID) . '" />';
+                    $output .= '<param name="quality" value="high"/>';
+                    $output .= '<param name="wmode" value="opaque" />';
 
-            if(get_field('swf_video', $post->ID)){
-                $output .= '';
-                $output .= '<object type="application/x-shockwave-flash"';
-                $output .= 'data="'.get_field('swf_video', $post->ID).'"';
-                $output .= 'width="'.get_field('swf_width', $post->ID).'" height="'.get_field('swf_height', $post->ID).'">';
-                $output .= '<param name="movie" value="'.get_field('swf_video', $post->ID).'" />';
-                $output .= '<param name="quality" value="high"/>';
-                $output .= '<param name="wmode" value="opaque" />';
+                    $output .= '</object>';
+                } else
+                {
+                    $output .= '<iframe class="medivision post__content--video__iframe" style="margin:0 auto; width: 398px; height:223px; display: block" src="http://www.medivision.co.uk/Dental/webpakonline.php?id=' . $vid_id . '" frameborder="0" marginwidth="1" marginheight="1" scrolling="no" ></iframe>';
+                }
 
-                $output .= '</object>';
-            }else{
-                $output .= '<iframe class="medivision post__content--video__iframe" style="margin:0 auto; width: 398px; height:223px; display: block" src="http://www.medivision.co.uk/Dental/webpakonline.php?id='.$vid_id.'" frameborder="0" marginwidth="1" marginheight="1" scrolling="no" ></iframe>';
+
+                $output .= '</div>';
+                $output .= (is_user_logged_in() == false) ? '</div>' : '</div>';
             }
-
-
-
-            $output .= '</div>';
-            $output .= (is_user_logged_in() == false)? '</div>' : '</div>';
         endwhile;
       endif;
       wp_reset_postdata();
