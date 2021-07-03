601030ea5cfd38841a0ab79d2ec8a6293a45adc4
Robert Kinsella <rob@kinsella.dev>
2021-07-03 19:34:11 +0100

    Main footer area style changes

--- templates/snippets/sidebar-footer-content.php
+++ templates/snippets/sidebar-footer-content.php
@@ -5,7 +5,7 @@
  * @package ac inuk
  */
 ?>
-<div id="quaternary" class="content__widget-area--footer sidebar" role="complementary">
+<div class="content__widget-area--footer sidebar" role="complementary">
   <div class="widget-area" >
     <?php if (!dynamic_sidebar('footer-aside')) : ?><?php endif; // end sidebar widget area ?>
   </div>
