df79de9d9feae884569e24f5304178ec6cd8a116
Richie Arnold <richard@ambercouch.co.uk>
2023-06-30 10:56:54 +0100

    CDC - added functions to remove page title

--- lib/ac-acf.php
+++ lib/ac-acf.php
@@ -307,3 +307,50 @@ if( function_exists('acf_add_local_field_group') ):
     ));
 
 endif;
+
+if( function_exists('acf_add_local_field_group') ):
+
+    acf_add_local_field_group(array(
+        'key' => 'group_page_settings)',
+        'title' => 'Page Settings',
+        'fields' => array(
+            array(
+                'key' => 'field_hide_page_title',
+                'label' => 'Hide Page Title',
+                'name' => 'hide_page_title',
+                'type' => 'true_false',
+                'instructions' => 'If Hide is selected then then page title will be removed',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'message' => '',
+                'default_value' => 0,
+                'ui' => 1,
+                'ui_on_text' => 'Hide',
+                'ui_off_text' => 'Show',
+            ),
+        ),
+        'location' => array(
+            array(
+                array(
+                    'param' => 'post_type',
+                    'operator' => '==',
+                    'value' => 'page',
+                ),
+            ),
+        ),
+        'menu_order' => 0,
+        'position' => 'side',
+        'style' => 'default',
+        'label_placement' => 'top',
+        'instruction_placement' => 'label',
+        'hide_on_screen' => '',
+        'active' => true,
+        'description' => '',
+    ));
+
+endif;
--- templates/content-page.php
+++ templates/content-page.php
@@ -7,13 +7,31 @@
 ?>
 
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>" >
+  <?php
+  /*
+   * ACF option to remove the page heade
+   */
+  if (get_field('hide_page_title')) :?>
+  <!-- .page__header is hidden -->
+  <?php else : ?>
   <header class="page__header">
     <div class="header--article">
+      <?php
+      /*
+       * Change the heading markup to H2 if is home page
+       */
+      if (is_front_page()) : ?>
       <h1 class="header__title">
         <span class="title--article" ><?php the_title(); ?></span>
       </h1>
+      <?php else: ?>
+        <h2 class="header__title">
+          <span class="title--article" ><?php the_title(); ?></span>
+        </h2>
+      <?php endif ?>
     </div>
   </header><!-- /.page__header -->
+  <?php endif ?>
 
   <div class="page__content">
     <?php the_content(); ?>
