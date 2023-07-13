16c83ff9662c4feb96d3767b9ad6af5357cf4fdb
Richie Arnold <richard@ambercouch.co.uk>
2023-07-13 13:28:02 +0100

    CDC - Fix missing favicon

--- header.php
+++ header.php
@@ -56,6 +56,11 @@
 <script type="text/javascript" src="//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js" async></script>
 		<!-- End TrustBox script -->
 
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/favicon/favicon-16x16.png">
+    <link rel="manifest" href="favicon/manifest.json">
+
   </head>
 
   <body <?php body_class(defined('ICL_LANGUAGE_CODE') ? ICL_LANGUAGE_CODE : ''); ?> <?php ac_body_data(); ?>>
