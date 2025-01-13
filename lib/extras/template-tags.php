ac54e316e59be181df250c69a1559958afe75327
Richie Arnold <richard@ambercouch.co.uk>
2025-01-13 13:17:15 +0000

    CDC - update search navigation

--- lib/extras/template-tags.php
+++ lib/extras/template-tags.php
@@ -30,7 +30,7 @@ if (!function_exists('ac_inuk_content_nav')) :
     $nav_class = ( is_single() ) ? 'navigation-post' : 'navigation-paging';
     ?>
     <nav role="navigation" id="<?php echo esc_attr($nav_id); ?>" class="<?php echo $nav_class; ?>">
-      <h1 class="screen-reader-text"><?php _e('Post navigation', 'ac_inuk'); ?></h1>
+<!--      <h1 class="screen-reader-text">--><?php //_e('Post navigation', 'ac_inuk'); ?><!--</h1>-->
 
       <?php if (is_single()) : // navigation links for single posts  ?>
 
@@ -39,13 +39,14 @@ if (!function_exists('ac_inuk_content_nav')) :
 
       <?php elseif ($wp_query->max_num_pages > 1 && ( is_home() || is_archive() || is_search() )) : // navigation links for home, archive, and search pages  ?>
 
-        <?php if (get_next_posts_link()) : ?>
-          <div class="nav-previous"><?php next_posts_link(__('<span class="meta-nav">&larr;</span> Older posts', 'ac_inuk')); ?></div>
-        <?php endif; ?>
+
 
         <?php if (get_previous_posts_link()) : ?>
-          <div class="nav-next"><?php previous_posts_link(__('Newer posts <span class="meta-nav">&rarr;</span>', 'ac_inuk')); ?></div>
+          <div class="nav-next"><?php previous_posts_link(__('<span class="meta-nav">&larr;</span> Previous', 'ac_inuk')); ?></div>
         <?php endif; ?>
+          <?php if (get_next_posts_link()) : ?>
+          <div class="nav-previous"><?php next_posts_link(__('More <span class="meta-nav">&rarr;</span>', 'ac_inuk')); ?></div>
+          <?php endif; ?>
 
       <?php endif; ?>
 
